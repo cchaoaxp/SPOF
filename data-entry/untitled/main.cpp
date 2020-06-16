98247ded89a187f7b0245640fd4856e2c79e36ae
chenghou chao <cchao@localhost.localdomain>
2020-06-16 15:26:30 -0400

    Signed-off-by: chenghou chao <cchao@localhost.localdomain>

--- data-entry/untitled/main.cpp
+++ data-entry/untitled/main.cpp
@@ -7,7 +7,7 @@ class Immutable
 {
    public:
    Immutable() { val = 23; };
-   const int* const getVal() const { return &val; }
+   const int& getVal() const { return val; }
 
    private:
    int val;// (1)
@@ -18,13 +18,13 @@ int main()
 
    std::cout << std::endl;
 
-   const Immutable immu;
-   std::cout << "val: " << *immu.getVal() << std::endl;
-   const int *ref = immu.getVal();
+   Immutable immu;
+   std::cout << "val: " << immu.getVal() << std::endl;
+   const int ref = immu.getVal();
 //   *(int*)&ref = 1234;
    *(int *)ref = 1234;
 
-   std::cout << "change val: " << *immu.getVal() << std::endl;
+   std::cout << "change val: " << immu.getVal() << std::endl;
 
    std::cout << std::endl;
 }
