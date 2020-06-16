df64176aa7bcff7e05f121e0c8f2e3b4d107f93f
chenghou chao <cchao@localhost.localdomain>
2020-06-16 15:38:25 -0400

    Signed-off-by: chenghou chao <cchao@localhost.localdomain>

--- testScript.sh
+++ testScript.sh
@@ -7,9 +7,7 @@ if [ $HOSTNAME != "localhost.localdomain" ]; then
    echo "No match"
    exit 1
 fi
-TEST_DIR=QtBuild/UpdateTimeAppMain/
-QMAKE=/opt/Qt/5.15.0/gcc_64/bin/qmake
-PROJECT_DIR=../data-entry
+TEST_DIR=QtBuild/UpdateTimeApp/UpdateTimeAppMain/
 
 cd $TEST_DIR
 ./UpdateTimeAppMain -wait -o UpdateTimeTest.xml -xml
